07b0c56b34a8511c60ae1591962fa82e9715cbfb
Sai Pavan <saikolloju@Sais-MacBook-Air.local>
2025-05-02 22:30:23 -0500

    Added thre files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders your custom content', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/your expected text/i);
+  expect(element).toBeInTheDocument();
 });
+
