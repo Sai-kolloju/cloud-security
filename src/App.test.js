93a59994396d8f4e9db12c4d6bc41711ebefe9bb
Sai Pavan <saikolloju@Sais-MacBook-Air.local>
2025-05-02 22:33:15 -0500

    Added thre files

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders your custom content', () => {
+test('renders cloud security heading', () => {
   render(<App />);
-  const element = screen.getByText(/your expected text/i);
+  const element = screen.getByText(/welcome to cloud security/i);
   expect(element).toBeInTheDocument();
 });
-
